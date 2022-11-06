518b6927b2a656fa569058c7618fd6d04934f8ba
Arekkusu17 <fernn.alex@gmail.com>
2022-11-06 16:23:30 -0300

    add script to sum the input values of stickers qty

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,23 @@ circleImage.addEventListener("click", () => {
 });
 
 //SECOND CHALLENGE
+
+const qtyFirstSticker = document.querySelector("#qtyFirstSticker");
+const qtySecondSticker = document.querySelector("#qtySecondSticker");
+const qtyThirdSticker = document.querySelector("#qtyThirdSticker");
+const qtyTotalStickers = document.querySelector("#qtyTotalStickers");
+const tooManyStickers = document.querySelector("#tooManyStickers");
+const btnStickers = document.querySelector("#buttonStickers");
+
+btnStickers.addEventListener("click", () => {
+  const firstInput = parseInt(qtyFirstSticker.value);
+  const secondInput = parseInt(qtySecondSticker.value);
+  const thirdInput = parseInt(qtyThirdSticker.value);
+  const inputSum = firstInput + secondInput + thirdInput;
+
+  if (inputSum <= 10) {
+    qtyTotalStickers.textContent = `Total of stickers selected: ${inputSum}`;
+  } else {
+    qtyTotalStickers.textContent = "You have too many stickers";
+  }
+});
