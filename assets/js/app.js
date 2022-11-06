b4f17cad15469046b20711045c36ee8b617511cb
Arekkusu17 <fernn.alex@gmail.com>
2022-11-06 15:33:48 -0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+//FIRST CHALLENGE
+
+const circleImage = document.querySelector("#circleImg");
+
+circleImage.addEventListener("click", () => {
+  circleImage.classList.toggle("border-pink");
+});
+
+//SECOND CHALLENGE
